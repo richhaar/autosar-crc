df730de53f233f676bb41df81a091f0c428e6437
Richard Haar <richard.haar.12@ucl.ac.uk>
2021-10-23 17:24:17 +0100

    updated README with examples

--- README.md
+++ README.md
@@ -2,10 +2,90 @@
 
 A C++ implemented set of 8,16,32,64 bit cyclic redundancy check (CRC) functions conforming to the CRC spec given by [AUTOSAR](https://www.autosar.org/). Written in C++ and compiled as a 'C++ addon' for use in JavaScript.
 
-Accepts the following types:
+## Getting Started
+
+These instructions will get you up and running using the autosar-crc node module on your local machine.
+
+### Prerequisites
+
+What things you need to install the software and how to install them
+
+* NodeJS
+* npm
+
+### Installing
+
+Autosar-crc is available on the npm registry, so within your console you can install `autosar-crc`.
+
+```
+npm install autosar-crc
+```
+
+### Different types of CRC
+
+Autosar-crc provides the following:
+
+```js example.js
+const autocrc = require('autosar-crc');
+
+console.log(autocrc.crc8('123456789'));
+console.log(autocrc.crc8_h2f('123456789'));
+console.log(autocrc.crc16('123456789'));
+console.log(autocrc.crc32('123456789'));
+console.log(autocrc.crc32_p4('123456789'));
+console.log(autocrc.crc64('123456789'));
+
+```
+```
+node example.js 
+```
+
+The polynomials of the CRC match the spec given in the AUTOSAR standard, see `crc_tables.h` for specifics on the polynomials.
+
+Each CRC accepts the following types:
 
 * `String`
 * `TypedArray`
 * `ArrayBuffer`
 
-Tested with [Jest](https://jestjs.io/)
+```js
+const autocrc = require('autosar-crc');
+
+const arr = new Uint8Array([0x31,0x32,0x33,0x34,0x35,0x36,0x37,0x38,0x39]);
+
+console.log(autocrc.crc8(arr));
+console.log(autocrc.crc8(arr.buffer));
+console.log(autocrc.crc8('123456789'))
+
+```
+
+## Running the tests
+
+The tests are found in the `main.test.js` file and are run with `Jest`.
+
+### Break down into end to end tests
+
+The test script is defined in `package.json` so you can run:
+
+```
+npm test
+```
+
+## Built With
+
+* [Node Addon API](https://github.com/nodejs/node-addon-api) - Module to use the Node-API from C++
+* [Jest](https://jestjs.io/) - Testing framework
+
+## Authors
+
+* **Richard Haar** - *Initial work* - [richhaar](https://github.com/richhaar)
+
+See also the list of [contributors](https://github.com/your/project/contributors) who participated in this project.
+
+## License
+
+This project is licensed under the ISC License - see the [LICENSE.md](LICENSE.md) file for details
+
+## Acknowledgments
+
+* Bastian Molkenthin for the table generation [here](http://www.sunshine2k.de/coding/javascript/crc/crc_js.html)
--- src/main.test.js
+++ src/main.test.js
@@ -1,4 +1,4 @@
-const functions = require('./index.js'); 
+const autocrc = require('./index.js'); 
 
 const row_1 = new Uint8Array([0x00, 0x00, 0x00, 0x00]);
 const row_2 = new Uint8Array([0xF2, 0x01, 0x83]);
@@ -11,87 +11,87 @@ const row_7 = new Uint8Array([0xFF, 0xFF, 0xFF, 0xFF]);
 const check_str = '123456789';
 
 test('crc8 check matches AUTOSAR table', () => {
-  expect(functions.crc8(row_1)).toBe(0x59);
-  expect(functions.crc8(row_2)).toBe(0x37);
-  expect(functions.crc8(row_3)).toBe(0x79);
-  expect(functions.crc8(row_4)).toBe(0xB8);
-  expect(functions.crc8(row_5)).toBe(0xCB);
-  expect(functions.crc8(row_6)).toBe(0x8C);
-  expect(functions.crc8(row_7)).toBe(0x74);
+  expect(autocrc.crc8(row_1)).toBe(0x59);
+  expect(autocrc.crc8(row_2)).toBe(0x37);
+  expect(autocrc.crc8(row_3)).toBe(0x79);
+  expect(autocrc.crc8(row_4)).toBe(0xB8);
+  expect(autocrc.crc8(row_5)).toBe(0xCB);
+  expect(autocrc.crc8(row_6)).toBe(0x8C);
+  expect(autocrc.crc8(row_7)).toBe(0x74);
 
-  expect(functions.crc8(check_str)).toBe(0x4B);
+  expect(autocrc.crc8(check_str)).toBe(0x4B);
 });
 
 test('crc8h2f check matches AUTOSAR table', () => {
-  expect(functions.crc8_h2f(row_1)).toBe(0x12);
-  expect(functions.crc8_h2f(row_2)).toBe(0xC2);
-  expect(functions.crc8_h2f(row_3)).toBe(0xC6);
-  expect(functions.crc8_h2f(row_4)).toBe(0x77);
-  expect(functions.crc8_h2f(row_5)).toBe(0x11);
-  expect(functions.crc8_h2f(row_6)).toBe(0x33);
-  expect(functions.crc8_h2f(row_7)).toBe(0x6C);
+  expect(autocrc.crc8_h2f(row_1)).toBe(0x12);
+  expect(autocrc.crc8_h2f(row_2)).toBe(0xC2);
+  expect(autocrc.crc8_h2f(row_3)).toBe(0xC6);
+  expect(autocrc.crc8_h2f(row_4)).toBe(0x77);
+  expect(autocrc.crc8_h2f(row_5)).toBe(0x11);
+  expect(autocrc.crc8_h2f(row_6)).toBe(0x33);
+  expect(autocrc.crc8_h2f(row_7)).toBe(0x6C);
 
-  expect(functions.crc8_h2f(check_str)).toBe(0xDF);
+  expect(autocrc.crc8_h2f(check_str)).toBe(0xDF);
 });
 
 test('crc16 check matches AUTOSAR table', () => {
-  expect(functions.crc16(row_1)).toBe(0x84C0);
-  expect(functions.crc16(row_2)).toBe(0xD374);
-  expect(functions.crc16(row_3)).toBe(0x2023);
-  expect(functions.crc16(row_4)).toBe(0xB8F9);
-  expect(functions.crc16(row_5)).toBe(0xF53F);
-  expect(functions.crc16(row_6)).toBe(0x0745);
-  expect(functions.crc16(row_7)).toBe(0x1D0F);
+  expect(autocrc.crc16(row_1)).toBe(0x84C0);
+  expect(autocrc.crc16(row_2)).toBe(0xD374);
+  expect(autocrc.crc16(row_3)).toBe(0x2023);
+  expect(autocrc.crc16(row_4)).toBe(0xB8F9);
+  expect(autocrc.crc16(row_5)).toBe(0xF53F);
+  expect(autocrc.crc16(row_6)).toBe(0x0745);
+  expect(autocrc.crc16(row_7)).toBe(0x1D0F);
 
-  expect(functions.crc16(check_str)).toBe(0x29B1);
+  expect(autocrc.crc16(check_str)).toBe(0x29B1);
 });
 
 test('crc32 check matches AUTOSAR table', () => {
-  expect(functions.crc32(row_1)).toBe(0x2144DF1C);
-  expect(functions.crc32(row_2)).toBe(0x24AB9D77);
-  expect(functions.crc32(row_3)).toBe(0xB6C9B287);
-  expect(functions.crc32(row_4)).toBe(0x32A06212);
-  expect(functions.crc32(row_5)).toBe(0xB0AE863D);
-  expect(functions.crc32(row_6)).toBe(0x9CDEA29B);
-  expect(functions.crc32(row_7)).toBe(0xFFFFFFFF);
+  expect(autocrc.crc32(row_1)).toBe(0x2144DF1C);
+  expect(autocrc.crc32(row_2)).toBe(0x24AB9D77);
+  expect(autocrc.crc32(row_3)).toBe(0xB6C9B287);
+  expect(autocrc.crc32(row_4)).toBe(0x32A06212);
+  expect(autocrc.crc32(row_5)).toBe(0xB0AE863D);
+  expect(autocrc.crc32(row_6)).toBe(0x9CDEA29B);
+  expect(autocrc.crc32(row_7)).toBe(0xFFFFFFFF);
 
-  expect(functions.crc32(check_str)).toBe(0xCBF43926);
+  expect(autocrc.crc32(check_str)).toBe(0xCBF43926);
 });
 
 test('crc32_p4 check matches AUTOSAR table', () => {
-  expect(functions.crc32_p4(row_1)).toBe(0x6FB32240);
-  expect(functions.crc32_p4(row_2)).toBe(0x4F721A25);
-  expect(functions.crc32_p4(row_3)).toBe(0x20662DF8);
-  expect(functions.crc32_p4(row_4)).toBe(0x9BD7996E);
-  expect(functions.crc32_p4(row_5)).toBe(0xA65A343D);
-  expect(functions.crc32_p4(row_6)).toBe(0xEE688A78);
-  expect(functions.crc32_p4(row_7)).toBe(0xFFFFFFFF);
+  expect(autocrc.crc32_p4(row_1)).toBe(0x6FB32240);
+  expect(autocrc.crc32_p4(row_2)).toBe(0x4F721A25);
+  expect(autocrc.crc32_p4(row_3)).toBe(0x20662DF8);
+  expect(autocrc.crc32_p4(row_4)).toBe(0x9BD7996E);
+  expect(autocrc.crc32_p4(row_5)).toBe(0xA65A343D);
+  expect(autocrc.crc32_p4(row_6)).toBe(0xEE688A78);
+  expect(autocrc.crc32_p4(row_7)).toBe(0xFFFFFFFF);
 
-  expect(functions.crc32_p4(check_str)).toBe(0x1697D06A);
+  expect(autocrc.crc32_p4(check_str)).toBe(0x1697D06A);
 });
 
 test('crc64 check matches AUTOSAR table', () => {
-  expect(functions.crc64(row_1)).toBe(0xF4A586351E1B9F4B);
-  expect(functions.crc64(row_2)).toBe(0x319C27668164F1C6);
-  expect(functions.crc64(row_3)).toBe(0x54C5D0F7667C1575);
-  expect(functions.crc64(row_4)).toBe(0xA63822BE7E0704E6);
-  expect(functions.crc64(row_5)).toBe(0x701ECEB219A8E5D5);
-  expect(functions.crc64(row_6)).toBe(0x5FAA96A9B59F3E4E);
-  expect(functions.crc64(row_7)).toBe(0xFFFFFFFF00000000);
+  expect(autocrc.crc64(row_1)).toBe(0xF4A586351E1B9F4B);
+  expect(autocrc.crc64(row_2)).toBe(0x319C27668164F1C6);
+  expect(autocrc.crc64(row_3)).toBe(0x54C5D0F7667C1575);
+  expect(autocrc.crc64(row_4)).toBe(0xA63822BE7E0704E6);
+  expect(autocrc.crc64(row_5)).toBe(0x701ECEB219A8E5D5);
+  expect(autocrc.crc64(row_6)).toBe(0x5FAA96A9B59F3E4E);
+  expect(autocrc.crc64(row_7)).toBe(0xFFFFFFFF00000000);
 
-  expect(functions.crc64(check_str)).toBe(0x995DC9BBDF1939FA);
+  expect(autocrc.crc64(check_str)).toBe(0x995DC9BBDF1939FA);
 });
 
 test('crc accepts string', () => {
-	expect(functions.crc64('123456789')).toBe(0x995DC9BBDF1939FA);
+	expect(autocrc.crc64('123456789')).toBe(0x995DC9BBDF1939FA);
 });
 
 test('crc accepts ArrayBuffer', () => {
 	const buf = new Uint8Array([0x31,0x32,0x33,0x34,0x35,0x36,0x37,0x38,0x39]).buffer;
-	expect(functions.crc64(buf)).toBe(0x995DC9BBDF1939FA);
+	expect(autocrc.crc64(buf)).toBe(0x995DC9BBDF1939FA);
 });
 
 test('crc accepts TypedArray', () => {
 	const arr = new Uint8Array([0x31,0x32,0x33,0x34,0x35,0x36,0x37,0x38,0x39]);
-	expect(functions.crc64(arr)).toBe(0x995DC9BBDF1939FA);
+	expect(autocrc.crc64(arr)).toBe(0x995DC9BBDF1939FA);
 });
